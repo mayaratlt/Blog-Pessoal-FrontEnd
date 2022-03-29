d9f570a30b4e83bd80b2e67afbd2be4181c1e380
mayaratlt <mayaratlt23@gmail.com>
2022-03-29 17:11:25 -0300

    Atualização blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,48 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch,  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario'
-import {Grid} from '@material-ui/core';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch> 
-        <div style={{minHeight: '100vh'}}>
+        <Switch>
+          <div style={{minHeight: '100vh'}}>
 
-          <Route exact path='/'>
-            <Login />
+            <Route exact path='/'>
+              <Login />
+            </Route>
 
-          </Route>
-          <Route path='/login'>
-            <Login />
+            <Route path='/login'>
+              <Login />
+            </Route>
 
-          </Route>
-          <Route path='/home'>
-            <Home />
-
-          </Route>
-          <Route path='/cadastrousuario'>
-            <CadastroUsuario />
-
-          </Route>
-        </div>
-      </Switch>
-      <Footer/>
+            <Route path='/home'>
+              <Home />
+            </Route>
+            
+            <Route path='/cadastrousuario'>
+              <CadastroUsuario />
+            </Route>
+            <Route path='/temas'>
+              <ListaTema />
+            </Route>
+            <Route path='/posts'>
+              <ListaPostagem />
+            </Route>
+          </div>
+        </Switch>
+      <Footer />
     </Router>
   );
 }
 
 export default App;
-
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,29 +1,28 @@
-import {Grid, Box, Typography, Button} from "@material-ui/core";
+import React from 'react';
+import {Typography, Box, Grid, Button} from '@material-ui/core';
+import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
+import './Home.css';
 
-//import {Box, Paper, Button} from "@material-ui/core";
-
-import "./Home.css";
-
-
-function Home(){
+function Home() {
     return (
         <>
-           <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo' > Seja bem Vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo' >Expresse aqui os seus pensamentos e opiniões!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
-                        <Box marginRight={1} >
+                        <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" className='botao'>Ver Postagens </Button>
+                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img src="https://i0.wp.com/derzweifel.com/wp-content/uploads/2021/04/frozen-2-recensione.jpg?fit=1920%2C1080&ssl=1" alt="Imagem Aula" width="900px" height="500px" />
+                    <img src="https://i0.wp.com/derzweifel.com/wp-content/uploads/2021/04/frozen-2-recensione.jpg?fit=1920%2C1080&ssl=1" alt="" width="500px" height="500px" />
                 </Grid>
                 <Grid xs={12} className='postagens'>
+                    <TabPostagem />
                 </Grid>
             </Grid>
         </>
