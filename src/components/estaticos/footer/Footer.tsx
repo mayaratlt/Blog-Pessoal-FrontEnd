6472c20e9e8a12b28dee3363d38c05a701dd1245
mayaratlt <mayaratlt23@gmail.com>
2022-04-04 17:25:26 -0300

    Atualização blog

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -3,45 +3,52 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Box, Grid } from '@material-ui/core';
-import './Footer.css'
+import './Footer.css';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
 
 function Footer() {
+
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    var footerComponent;
+
+    if(token != ""){
+        footerComponent = <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Grid  alignItems="center" item xs={12}>
+            <Box style={{ backgroundColor: "#495057", height: "50px"}}>
+                    <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
+                </Box>
+                <Box className="menu3" display="flex" alignItems="center" justifyContent="center">
+                    <a href="https://www.facebook.com/mayara.talyta/" target="_blank">
+                        <FacebookIcon className='redes' />
+                    </a>
+                    <a href="https://www.instagram.com/mayaratalyta/" target="_blank">
+                        <InstagramIcon className='redes' />
+                    </a>
+                    <a href="https://www.linkedin.com/in/mayaraalmeida24/" target="_blank">
+                        <LinkedInIcon className='redes' />
+                    </a>
+                </Box>
+            
+            <Box style={{ backgroundColor: "#495057", height: "60px"}}>
+                <Box paddingTop={1} className="menu3">
+                    <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2020 Copyright:</Typography>
+                </Box>
+                <Box>
+                    <a target="_blank" href="https://brasil.generation.org">
+                        <Typography  variant="subtitle2" gutterBottom className='textos'  align="center">brasil.generation.org</Typography>
+                    </a>
+                </Box>
+            </Box>
+        </Grid>
+    </Grid>
+    }
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box className='box1'>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom className='testos'>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/mayara.talyta/" target="_blank">
-                                <FacebookIcon  className='redes' />
-                            </a>
-                            <a href="https://www.instagram.com/mayaratalyta/" target="_blank">
-                                <InstagramIcon className='redes' />
-                            </a>
-                            <a href="https://www.linkedin.com/in/mayaraalmeida24/" target="_blank">
-                                <LinkedInIcon className='redes' />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box className='box2'>
-                        constructor(parameters) {
-                            
-                        }
-                
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom className='testos' >© 2020 Copyright:</Typography>
-                        </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom className='testos' align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
+            {footerComponent}
         </>
     )
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -4,12 +4,16 @@ import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 import './Home.css';
 import { useHistory } from 'react-router';
-import useLocalStorage from 'react-use-localstorage';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../store/tokens/tokensReducer';
+import { Link } from 'react-router-dom';
 
 function Home() {
 
     let history = useHistory();
-    const [token, setToken] = useLocalStorage('token');
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
     
     useEffect(() => {
       if (token == "") {
@@ -30,11 +34,13 @@ function Home() {
                         <Box marginRight={1}>
                             <ModalPostagem />
                         </Box>
-                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                        </Link>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img src="https://github.com/joaocarloslima/portifolio/blob/master/imagens/bg.jpg?raw=true" alt="" width="950px" height="600px" />
+                    <img src="https://github.com/joaocarloslima/portifolio/blob/master/imagens/bg.jpg?raw=true" alt="" width="950px" height="650px" />
                 </Grid>
                 <Grid xs={12} className='postagens'>
                     <TabPostagem />
